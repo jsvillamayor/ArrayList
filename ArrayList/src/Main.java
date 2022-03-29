be32cca8deca0217142427d5009f4e56a1d1e89d
jsvillamayor <78101377+jsvillamayor@users.noreply.github.com>
2022-03-29 16:29:00 -0700

    Add files via upload

---
+++ ArrayList/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> food = new ArrayList<String>();
+		
+		food.add("pizza");
+		food.add("ramen");
+		food.add("pasta");
+		
+		//food.set(0, "sushi");
+		//food.remove(2);
+		//food.clear();
+		
+		for(int i=0;i<food.size(); i++) {
+			System.out.println(food.get(i));
+		}
+
+	}
+
+}
